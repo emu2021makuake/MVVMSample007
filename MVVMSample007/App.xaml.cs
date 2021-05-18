df066ba47305a929fdfe3286e9c046b6667dc445
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-05-18 18:53:35 +0900

    Add files via upload

---
+++ MVVMSample007/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace MVVMSample007
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ MVVMSample007/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MVVMSample007.ViewModels;
+using System.Windows;
+
+namespace MVVMSample007.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
